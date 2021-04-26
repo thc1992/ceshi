803944e123a43da2c08807285731cfac4091064d
tang <tanghongchao@uniontech.com>
2021-04-26 15:22:56 +0800

    11

--- Yaml/main.py
+++ Yaml/main.py
@@ -3,4 +3,4 @@ import os
 
 if __name__ == '__main__':
     pytest.main(['-s', '-q', '--alluredir', './allure-results',
-                 os.os.path.dirname(os.path.abspath(__file__)) + "/Yaml/Test_001_01.py"])
+                 os.path.dirname(os.path.abspath(__file__)) + "/Yaml/Test_001_01.py"])
