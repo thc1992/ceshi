9e126e91c1f9d025701ea275e133b39fbbcfb187
sky <709869467@qq.com>
2023-12-11 17:45:15 +0800

    Update main.py

--- Yaml/main.py
+++ Yaml/main.py
@@ -2,5 +2,5 @@ import pytest
 import os
 
 if __name__ == '__main__':
-    pytest.main(['-s', '-q', '--alluredir', os.path.dirname(os.path.abspath(__file__)) + '/../allure-results',
+    pytest.main(['-s', '-v', '--alluredir', os.path.dirname(os.path.abspath(__file__)) + '/../allure-results',
                  os.path.dirname(os.path.abspath(__file__)) + "/Test_001_01.py"])
