e0a0279265779e9e662671b9b071ae207df1b47d
sky <709869467@qq.com>
2023-12-12 11:40:06 +0800

    Update Test_001_01.py

--- Yaml/Test_001_01.py
+++ Yaml/Test_001_01.py
@@ -2,7 +2,7 @@ import pytest, allure
 import os
 
 
-@allure.feature('测试用例功能')  # feature定义功能
+@allure.feature('测试用例功能') 
 class Test_Class(object):
 
     @pytest.fixture(scope='function')
@@ -10,27 +10,26 @@ class Test_Class(object):
         def teardown_function():
             print("teardown_function called.")
 
-        request.addfinalizer(teardown_function)  # 此内嵌函数做teardown工作
+        request.addfinalizer(teardown_function)
         print('setup_function called.')
 
     @pytest.fixture(scope='module')
     def setup_module(request):
         def teardown_module():
             print("teardown_module called.")
-
         request.addfinalizer(teardown_module)
         print('setup_module called.')
 
-    @allure.story('功能测试用例1')  # story定义用户场景
+    @allure.story('11111111111111111111')  
     @pytest.mark.website
     def test_1(setup_function):
         print('Test_1 called.')
 
-    @allure.story('功能测试用例2')  # story定义用户场景
+    @allure.story('22222222222222') 
     def test_2(setup_module):
         print('Test_2 called.')
 
-    @allure.story('功能测试用例3')  # story定义用户场景
+    @allure.story('333333333333333333333') 
     def test_3(setup_module):
         print('Test_3 called.')
         assert 2== 1 + 1
