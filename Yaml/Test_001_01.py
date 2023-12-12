ca9842766f5ed10de2cc9f67c29af5934ab0cf7c
sky <709869467@qq.com>
2023-12-12 11:46:49 +0800

    Update Test_001_01.py

--- Yaml/Test_001_01.py
+++ Yaml/Test_001_01.py
@@ -5,23 +5,7 @@ import os
 @allure.feature('测试用例功能') 
 class Test_Class(object):
 
-    @pytest.fixture(scope='function')
-    def setup_function(request):
-        def teardown_function():
-            print("teardown_function called.")
-
-        request.addfinalizer(teardown_function)
-        print('setup_function called.')
-
-    @pytest.fixture(scope='module')
-    def setup_module(request):
-        def teardown_module():
-            print("teardown_module called.")
-        request.addfinalizer(teardown_module)
-        print('setup_module called.')
-
     @allure.story('11111111111111111111')  
-    @pytest.mark.website
     def test_1(setup_function):
         print('Test_1 called.')
 
